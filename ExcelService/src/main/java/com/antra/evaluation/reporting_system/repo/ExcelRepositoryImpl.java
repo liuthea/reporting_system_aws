603469e34d7fcb7239c14c30bb72d74c64f5d5e0
liuthea <59734799+liuthea@users.noreply.github.com>
2021-07-22 22:31:17 -0400

    trivial

--- ExcelService/src/main/java/com/antra/evaluation/reporting_system/repo/ExcelRepositoryImpl.java
+++ ExcelService/src/main/java/com/antra/evaluation/reporting_system/repo/ExcelRepositoryImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public class ExcelRepositoryImpl implements ExcelRepository {
